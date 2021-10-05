4d25db65b23f3acc9da7be9bbb5627e06d99e863
Almadavic <almadavic@live.com>
2021-10-05 12:09:03 -0300

    Seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,82 +1,102 @@
 package model.entities;
 
-import java.sql.Date;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
-public class Seller {
-private Integer id;
-private String name;
-private String emailString;
-private Date birthDate;
-private Double baseSalary;
-private Department department;
+public class Seller implements Serializable {
 
-public Seller() {
-	
-}
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String emailString;
+	private Date birthDate;
+	private Double baseSalary;
+	private Department department;
 
+	public Seller() {
 
+	}
 
+	public Seller(Integer id, String name, String emailString, Date birthDate, Double baseSalary,
+			Department department) {
+		this.id = id;
+		this.name = name;
+		this.emailString = emailString;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
 
-public Seller(Integer id, String name, String emailString, Date birthDate, Double baseSalary, Department department) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.emailString = emailString;
-	this.birthDate = birthDate;
-	this.baseSalary = baseSalary;
-	this.department = department;
-}
+	public Integer getId() {
+		return id;
+	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public Integer getId() {
-	return id;
-}
+	public String getEmailString() {
+		return emailString;
+	}
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
 
-public String getName() {
-	return name;
-}
+	public Date getBirthDate() {
+		return birthDate;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 
-public String getEmailString() {
-	return emailString;
-}
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
 
-public void setEmailString(String emailString) {
-	this.emailString = emailString;
-}
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
 
-public Date getBirthDate() {
-	return birthDate;
-}
+	public Department getDepartment() {
+		return department;
+	}
 
-public void setBirthDate(Date birthDate) {
-	this.birthDate = birthDate;
-}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 
-public Double getBaseSalary() {
-	return baseSalary;
-}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-public void setBaseSalary(Double baseSalary) {
-	this.baseSalary = baseSalary;
-}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
 
-public Department getDepartment() {
-	return department;
-}
-
-public void setDepartment(Department department) {
-	this.department = department;
-}
-
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", emailString=" + emailString + ", birthDate=" + birthDate
+				+ ", baseSalary=" + baseSalary + ", department=" + department + "]";
+	}
 
 }
