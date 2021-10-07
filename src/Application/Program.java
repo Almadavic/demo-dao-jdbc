34ad4aeee7b3f260922e08c63dbe929395de9f8a
Almadavic <almadavic@live.com>
2021-10-07 01:06:30 -0300

    implements insert

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -27,5 +27,11 @@ public static void main(String[] args) {
 	for(Seller obj : list) {
 		System.out.println(obj);
 	}
+	
+	System.out.println("\n=== TEST4 : seller Insert===");
+	Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
+	sellerDao.insert(newSeller);
+	System.out.println("Inserted ! New id = "+newSeller.getId());
+	
    }
 }
